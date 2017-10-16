b0b43be14ca4f4c90fadbf08f771418781d43790
akaustav <akaustav@gmail.com>
2017-10-15 17:20:32 -0700

    Reads user interation with the physical buttons

--- thermostat.ino
+++ thermostat.ino
@@ -3,6 +3,9 @@
 // v0.0.2
 // ------------------------------------------
 
+#define BUTTON_PRESSED_TIMEOUT 7000 // 7 seconds
+#define DEBOUNCE_DELAY_PLUS  500000 // 500 milliseconds
+#define DEBOUNCE_DELAY_MINUS 500000 // 500 milliseconds
 
 // Declare aliases for pins
 //   temperature_plus is on pin D2
@@ -10,13 +13,20 @@
 int temperature_plus = D2;
 int temperature_minus = D6;
 
-// Declare global variables for setting temperature
+// Declare global variables
+// Variables for button presses
+int changeWindow = 0; // Are you in the window change? Is the timer running?
+int counter = 76;
+
+// Variable for setting temperature
 int oldSetToTemp = 76;  // Previous set to temperature value is assumed as 76° Fahrenheit
 int newSetToTemp = 76;  // Default set to temperature value is 76° Fahrenheit
 int correction = 0;  // Positive difference between oldSetToTemp and newSetToTemp
 
 // char setTime[6] = "HH:MM";
 
+// Initialize a timer
+Timer buttonPressedTimer(BUTTON_PRESSED_TIMEOUT, resetButtonPressedFlag);
 
 // Function for initial setup of the device
 void setup() {
@@ -27,6 +37,10 @@ void setup() {
   // INPUT consumes less power than OUTPUT
   pinMode(temperature_plus, INPUT);
   pinMode(temperature_minus, INPUT);
+
+  // Setting up interrupts
+  attachInterrupt(temperature_plus, plus_button_pressed, FALLING);
+  attachInterrupt(temperature_minus, minus_button_pressed, FALLING);
 
   // Register Particle.functions
   // When we ask the cloud for the function "SetTempTo", it will employ the function set_temperature_app() from this app.
@@ -39,8 +53,57 @@ void setup() {
   // This variable is used for tracking current set to temperture
   Particle.variable("NewSetTo", newSetToTemp);
 
+  Particle.variable("Counter", counter);
+  Particle.variable("changeWindow", changeWindow);
+
   // This variable is used to read the timestamp
   // Particle.variable("Time", setTime);
+
+  // Enable all interrupts
+  interrupts();
+}
+
+
+// Callback function for plus button pressed interrupt
+void plus_button_pressed() {
+  // noInterrupts();
+  buttonPressedTimer.reset();
+  ATOMIC_BLOCK() {
+    delayMicroseconds(DEBOUNCE_DELAY_PLUS); // Wait for DEBOUNCE_DELAY
+  }
+  counter++;
+
+  if (changeWindow == 0) {
+    changeWindow = 1;
+  } else if (changeWindow == 1) {
+    oldSetToTemp++;
+  }
+
+  // interrupts();
+}
+
+// Callback function for plus button pressed interrupt
+void minus_button_pressed() {
+  // noInterrupts();
+  buttonPressedTimer.reset();
+  ATOMIC_BLOCK() {
+    delayMicroseconds(DEBOUNCE_DELAY_MINUS); // Wait for DEBOUNCE_DELAY
+  }
+  counter--;
+
+  if (changeWindow == 0) {
+    changeWindow = 1;
+  } else if (changeWindow == 1) {
+    oldSetToTemp--;
+  }
+
+  // interrupts();
+}
+
+// Timer reset callback
+void resetButtonPressedFlag() {
+  buttonPressedTimer.stop();
+  changeWindow = 0;
 }
 
 
